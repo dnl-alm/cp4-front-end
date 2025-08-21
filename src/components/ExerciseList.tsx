787fc80caa7f6dac6d588afa9027700fe9a9a25b
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-20 22:05:48 -0300

    feat: Created Exercise List

---
+++ src/components/ExerciseList.tsx
@@ -0,0 +1,27 @@
+export function ExerciseList() {
+  return (
+    <>
+    <div style={{ border: '0.3rem solid #000000', padding: "0px", width: "20rem", height: "10rem" }}>
+      <h2>Exercicio Maromba 1</h2>
+      <p>Tipo: Flexão</p>
+      <p>repetições: 100</p>
+      <p>Intensidade: Maxima</p>
+    </div>
+
+    <div style={{ border: '0.3rem solid #000000', padding: "0px", width: "20rem", height: "10rem"}}>
+      <h2>Exercicios Maromba 2</h2>
+      <p>Tipo: Abdominais</p>
+      <p>repetições: 100</p>
+      <p>Intensidade: Maxima</p>
+    </div>
+
+    <div style={{ border: '0.3rem solid #000000', padding: "0px", width: "20rem", height: "10rem" }}>
+      <h2>Exercicios Maromba 3</h2>
+      <p>Tipo: Agachamento</p>
+      <p>repetições: 100</p>
+      <p>Intensidade: Maxima</p>
+    </div>
+    </>
+  );
+}
+
