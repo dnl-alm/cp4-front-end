caa576852dd967106b93df2aa22206708ad79b64
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-20 22:21:39 -0300

    feat: improved welcome

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -1,16 +1,17 @@
 export function Welcome() {
   return (
    <>
-   <div style={{
+    <div style={{
 
-    border: "0.3rem solid #000000",
-    borderRadius: '0.5rem'
+      border: "0.3rem solid #000000",
+      borderRadius: '0.5rem',
+      textAlign: 'center',
 
-   }}>
-   <h1>Bem Vindo ao</h1>
-   <h2>Trackfit-mini</h2>
-   <p>Trackfit o lugar onde você será mo mais forte no seu grupo de amigos🦾🦾🦾</p> 
-   </div>
+    }}>
+      <h1>Bem Vindo ao</h1>
+      <h2>Trackfit-mini</h2>
+      <p>Trackfit o lugar onde você será mo mais forte no seu grupo de amigos🦾🦾🦾</p> 
+    </div>
    </>
   )
 
